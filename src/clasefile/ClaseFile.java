964b3f2373495b58433deaba0aff450a94b175a3
ardark <ardark@ardark-pc>
2017-09-26 14:20:22 +0200

    Algunos pequeños cambios V1.1 Signed-off-by:ardark <ardark@ardark-pc>

--- src/clasefile/ClaseFile.java
+++ src/clasefile/ClaseFile.java
@@ -21,6 +21,8 @@ public class ClaseFile {
     public static void ComprobarDirectorio (String ruta)
     {
 	File archivo = new File(ruta);
+	InformacionArchivo(archivo);
+	
 	//Si la ruta es un directorio cojo todos sus hijos
 	if (archivo.isDirectory())
 	{
@@ -31,37 +33,28 @@ public class ClaseFile {
 		ComprobarArbol(a);
 	    }
 	}
-	else
-	{
-	    InformacionArchivo(archivo);
-	}	
     }
     
     //Este metodo se mete dentro de carpetas hasta que encuentra el archivo más profundo
-    public static File ComprobarArbol (File archivo)
+    public static void ComprobarArbol (File archivo)
     {
 	//Compruebo si el archivo es un documento
 	if (archivo.isFile())
 	{
-	    InformacionArchivo(archivo);
-	    //Termino el bucle
-	    return archivo;
+	    InformacionArchivo(archivo);	   
 	}
 	//Si el archivo es un directorio accede a los documentos, imprime y comprueba de nuevo
 	else
 	{
-	    //Imprimo el propio directorio 
-	    InformacionArchivo(archivo);
-	    
+	    //Imprimo la info del directorio 
+	    InformacionArchivo(archivo);	    
 	    //Saco sus hijos
-	    File [] listaDirectorio = archivo.listFiles();
-	    
-	    //Para cada archivo compruebo de nuevo que tiene dentro
+	    File [] listaDirectorio = archivo.listFiles();	    
+	    //Para cada hijo compruebo de nuevo que tiene dentro
 	    for (File a : listaDirectorio)
 	    {
 		ComprobarArbol(a);
 	    }
-	    return archivo;
 	}
     }
     
